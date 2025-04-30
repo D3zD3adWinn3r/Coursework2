a97e509a21484481ff82544993ff9b298ed4f0c3
D3zD3adWinn3r <Desireenlopez5@gmail.com>
2025-04-29 20:36:20 -0500

    Add files via upload

---
+++ Coursework2/Guided Practice/GP.js
@@ -0,0 +1,14 @@
+// This is a guided practice for the students to practice arrays and objects in JavaScript.
+// The students are given an array of students with their names, ages, and favorite subjects
+// and are asked to write a console log that reads "Megan is 16 and likes math".
+// The students are also given a hint to use the index of the array to access the values.
+const students = [['Joe', 'Megan', 'April'], [17,16,18], ["math", "english", "math"]]
+
+//write a console that reads Megan is 16 and likes nath using the array above
+
+
+const nameArray = ['Joe', 'Megan', 'April']
+const ageArray = [17, 16, 18]
+const subjectArray = ['math', 'english', 'math']
+console.log(subjectArray[2])
+console.log(`${students[0]} is ${students[1]} and likes ${students[2]}`)
---
+++ Coursework2/Unit2.DinnerParty/index.js
@@ -0,0 +1,23 @@
+// - You ate a cheeseburger for $12.
+// - Jonah had the $20 steak.
+// - Amy's soup cost $8.
+// - Mateo did not like his $14 mac and cheese.
+// - Cheyenne was impressed by the $16 unlimited soup and salad combo.
+
+
+const dinner = {
+    cheeseburger: 12,
+    steak: 20,
+    soup: 8,
+    mac: 14,
+    souandsalad: 16 
+}
+
+// 1. Declare an object named `dinner` where each key is the name of the purchased food, and the value is the corresponding price.
+// console.log(dinner)?
+// console.log(Object.values(dinner))?
+
+
+
+// 2. Print the names of each food in `dinner` twice: once using a loop, and then with `Object.keys`.
+// 3. Print the prices of each food in `dinner` twice: once using a loop, and then with `Object.values`.
