2b2202d6196fca8ceba40c9a519cfb332fdbb208
D3zD3adWinn3r <Desireenlopez5@gmail.com>
2025-05-01 18:42:51 -0500

    add

---
+++ prac.js
@@ -0,0 +1,65 @@
+//write a function that takes an array of numbers and a number and returns all of the numbers in the array multiplied by the given number
+
+function multiplyArray(arrofNums, num) {
+    //look at each number in the arrOfnum
+    //build a for loop
+    for(let i =0; i <arrofNums.length;i++) {
+    //inside our for loop mutilpli arrofNums [i] * num
+    const calculation = arrofNums[i] * num
+    newArray.push(calculations)
+    console.log(NewArray)
+    //newArray.push(arrofNums[i] * num)
+    //after calcutions, save it to a new array
+    }
+    return newArray
+    //outside the for loop, return the new array
+}
+console.log(multiplyArray(numSample, 10))    
+
+//write a function that takes an array of 'cart objects' add the prices of all the items in each object and add a new total prpoerty set eqaul to the calculation
+
+const obj= {}
+const sampleCarts+ []
+    {
+        item1:99,
+        item2:3,
+        item3:27,
+    }
+    {
+        item1:99,
+        item2:3,
+        item3:27,
+    } {
+        item1:99,
+        item2:3,
+        item3:27,
+    } {
+        item1:99,
+        item2:3,
+        item3:27,
+    }
+
+const addToatal = (arrOfCarts) => {
+    //builks a loop to look at each cart object in the arrofCarts
+    //inside my for loop i will use a for in loop to add all the prices in my object
+   //create a new property on the object (= arrofCarts[i]) and set it equal to the calculation
+   //return the orignia arrofcarytts after it been amnipulated
+
+//write a function that takes an array of strings and remove all the strings that start with vowels
+
+//write a function that takes an array of 'user' objects. return only the objects with an age property greater than 18
+
+//write a function that takes an array of numbers and a number and finds the number that was given in the array of numbers
+
+//write a function that takes an array of cart objects. return only the objects with an total less than 100 dollars
+
+//write a function that takes an array of 'user' objects. return a string with all of the users names i.e 'the users are...'
+
+//write a function that takes an array of 'user' objects. return an array with all the users names
+
+//write a function that takes a single 'user' object and adds an id.
+//write a for loop that applies the function to an array of 'user' objects
+
+const numSample = [17,37,1,79,23,8,67,14,66]
+const sampleStrings = ["Hello", "I'm happy!", "How are you?", "Are you leaving?", "Every", "Taco"]
+
